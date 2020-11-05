d7c6f6b8c542f6fde0720ff8bef668d3aad2ec7a
wajjee12 <wkhan25@uic.edu>
2020-11-05 17:46:35 -0600

    UpdatingTables

--- src/main/scala/MyTesting.scala
+++ src/main/scala/MyTesting.scala
@@ -1,32 +1,36 @@
 
 import java.security.MessageDigest
 
-import akka.actor.TypedActor.{lookup, self}
+import akka.actor.TypedActor.{context, lookup, self}
 import akka.actor.typed.scaladsl.adapter.ClassicActorRefOps
 import akka.actor.{Actor, ActorLogging, ActorPath, ActorRef, ActorSelection, ActorSystem, Props}
-import lookupdata.{ getValue, map}
+import lookupdata.{getValue, map}
 import lookupdata.create_fingertable
 
 import scala.collection.mutable
 import scala.util.control.Breaks.break
 //https://medium.com/@TamasPolgar/what-to-do-with-5-000-000-akka-actors-381a915a0f78 "referenced for storing data in node actor(hashmap)"
-object MyTesting extends App{
+object MyTesting extends App {
 
-  var list_of_names_to_Assign_to_node = List("Wajahat","Umar","Krunal","CS441","myname","yourname") //fake data stored in nodes actors
+  var list_of_names_to_Assign_to_node = List("Wajahat", "Umar", "Krunal", "CS441", "myname", "yourname") //fake data stored in nodes actors
 
 
-  case class UserActor(title:String, ref:ActorSelection,value:String)
-  case class ServerActor(title:String)
-  case class Insert(key:String,value: String)
-  case class get_value (key:String) //get the value
-  case class insert(key:String , Value:String) //key value pair
+  case class UserActor(title: String, ref: ActorSelection, value: String)
+
+  case class ServerActor(title: String)
+
+  case class Insert(key: String, value: String)
+
+  case class get_value(key: String) //get the value
+  case class insert(key: String, Value: String) //key value pair
   //case class add_node_to_ring(Key: String,ref:ActorRef ,path: ActorPath)
-  var map = new mutable.HashMap[String,String]() //stores key with the value of actor reference
+  var map = new mutable.HashMap[String, String]() //stores key with the value of actor reference
 
 
-  class akka extends Actor with ActorLogging{
-    var n=0;
-    def receive= {
+  class akka extends Actor with ActorLogging {
+    var n = 0;
+
+    def receive = {
       case UserActor(title, ref, value) =>
         // println(self)
         //  println(title)
@@ -35,7 +39,6 @@ object MyTesting extends App{
         ref ! lookupdata.Insert(title, value)
 
     }
-
 
 
   }
@@ -55,43 +58,54 @@ object MyTesting extends App{
   //creating a hash value
   val number_nodes = 4 //val defines a constant value which cannot be modified once declared
   val user_actor = 4
-  //create node_Actors
-  def MD5(s:String)={MessageDigest.getInstance("MD5").digest(s.getBytes())}
-  var counter=0
 
-  for(counter <- 1 to number_nodes) {
+  //create node_Actors
+  def MD5(s: String) = {
+    MessageDigest.getInstance("MD5").digest(s.getBytes())
+  }
+
+  var counter = 0
+
+  //for(counter <- 0 to 1)
+  {
     {
-      val node_Actors = system.actorOf(Props[lookupdata],"nodeactor"+counter)
-//      println("Node created for the ring:   " + node_Actors.path)
+      val node_Actors = system.actorOf(Props[lookupdata], "nodeactor" + 0)
+      //      println("Node created for the ring:   " + node_Actors.path)
       //create the ring here
       var to_behashed = counter.toString;
-      var hashValue=MD5("nodeactor"+to_behashed)
+      var hashValue = MD5("nodeactor" + to_behashed)
       //so the finger table will consist of node identifiers and the keys will be stored in the finger table
-
       //Now from here we will start updating the finger table
-      node_Actors!lookupdata.add_nodering(hashValue.toString,node_Actors.path,node_Actors.ref)
+      node_Actors ! lookupdata.add_nodering(counter.toString, node_Actors.path, node_Actors.ref)
+      val node_Actors1 = system.actorOf(Props[lookupdata], "nodeactor" + 1)
+      var to_behashed1 = counter + 1.toString;
+      var hashValue1 = MD5("nodeactor" + to_behashed1)
+
+      node_Actors1 ! lookupdata.add_nodering(counter + 1.toString, node_Actors1.path, node_Actors1.ref)
+    }
+
+
+    val user_actor = system.actorOf(Props[akka], "useractor" + counter)
+       for (counter <- 0 to number_nodes) {
+
+      val select_Actor = system.actorSelection("akka://UserServerActors/user/" + "nodeactor" + 0)
+      var to_behashed = counter.toString;
+      var hashValue = MD5("nodeactor" + to_behashed)
+      user_actor ! UserActor(counter.toString, select_Actor, list_of_names_to_Assign_to_node(counter)) //loading the data into node actors via useractor
+
     }
 
   }
-
-
-  for(counter <- 1 to number_nodes) {
-
-    val user_actor = system.actorOf(Props[akka],"useractor"+counter)
-    val select_Actor = system.actorSelection("akka://UserServerActors/user/"+"nodeactor"+counter)
-    var to_behashed = counter.toString;
-    var hashValue=MD5("nodeactor"+to_behashed)
-    user_actor ! UserActor(counter.toString,select_Actor,list_of_names_to_Assign_to_node(counter)) //loading the data into node actors via useractor
-  }
-
-  for(counter <- 1 to number_nodes) {
-
-    val select_Actor = system.actorSelection("akka://UserServerActors/user/"+"nodeactor"+counter)
-    var to_behashed = counter.toString;
-    var hashValue=MD5("nodeactor"+to_behashed)
-    select_Actor ! lookupdata.getValue(counter.toString)
-
-  }
-
 }
+//
+//  for(counter <- 1 to number_nodes) {
+//
+//    val select_Actor = system.actorSelection("akka://UserServerActors/user/"+"nodeactor"+counter)
+//    //var to_behashed = counter.toString;
+//    //var hashValue=MD5(to_behashed)
+//    select_Actor ! lookupdata.getValue(counter.toString)
+//
+//  }
+
+
 
--- src/main/scala/lookupdata.scala
+++ src/main/scala/lookupdata.scala
@@ -2,11 +2,14 @@ import java.lang.System.Logger
 
 import akka.actor.{Actor, ActorLogging, ActorPath, ActorRef, Props}
 import MyTesting.{MD5, UserActor, akka, list_of_names_to_Assign_to_node, map, system}
-import lookupdata.{Insert, add_nodering, create_fingertable, getValue}
+import lookupdata.{Insert, add_nodering, create_fingertable, getValue, get_fingertable, store_nodenumbers, store_temporary_value, update_finger_table}
 
+import scala.Int.{int2double, int2long}
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
 /*
-//Chord Implementation idea idea was taken from  "Mr.Abhijeet's " github repo.
+//Chord Implementation idea was from "Mr.Abhijeet"  .
 
 */
 object lookupdata{
@@ -15,47 +18,110 @@ object lookupdata{
   var map = new mutable.HashMap[String,String]()
   case class find_successor() ////the hashed key user request for will be looked into each of the finger_table and data will be retrieved from there
   case class find_predecessor()
-  case class update_finger_table() //updating finger table comes in when a new node joins so that you update in the current node
-  case class create_fingertable(totalnodes:Int) //finger table will contains the immediate successors (atleast 2) of our current node
+  case class update_finger_table(nodesnumber:ListBuffer[Int]) //updating finger table comes in when a new node joins so that you update in the current node
+  case class create_fingertable(totalnodes:Int,hashvalue:String,actorPath: ActorPath) //finger table will contains the immediate successors (atleast 2) of our current node
   var num_of_nodes_in_ring=0;
   case class add_nodering(Key:String,actorPath: ActorPath,ref: ActorRef)
+  var store_nodenumbers = new ListBuffer[Int]
+   var store_temporary_value = 0
+  case class get_fingertable()
 }
 
 class lookupdata extends Actor with ActorLogging {
   var actorspath = new mutable.ListBuffer[String] //List Buffer gives constant time
   var lookup_servernodes = new mutable.HashMap[String,String]
+  var counter =0
+  var i =0
+  var fingertablemap = new mutable.HashMap[Int,Int]()
   def receive ={
-    case create_fingertable(totalnodes) =>{
+    case create_fingertable(totalnodes,hashvalue,actorPath) =>{
+
+
       //so our approach is basically we will create finger table for each node
       //we also update the existing one(s) all the previous nodes created (node joining requires to keep track of the predecessor but we are
       // keeping track of all paths and with them we are retrieving finger tables and updating them
       // (this part was required in Project but we are doing it here)
         println("Generating finger table for Node: " + self.path)
-
       //generate finger table using the formula ("remember table contains the identifier and the key
+          for(counter <- 0 to  3){ //instead of 3 we will consider m-1
+            //counter represent the keys (or ID for our nodes) and the formula (value) is the identifier scala.math.pow(2, totalnodes))
+            //so the result of the formula will give us successor
+                //fingertablemap+=(counter->((Integer.parseInt(hashvalue)+scala.math.pow(2, counter)) % totalnodes).toInt) //6
+               //   println("Server Node is: " + (Integer.parseInt(hashvalue) + "  The key is: " + counter + " And the value is: "+ ((Integer.parseInt(hashvalue)+scala.math.pow(2, counter)) % scala.math.pow(2, totalnodes)).toInt))
+                    store_temporary_value=((Integer.parseInt(hashvalue)+scala.math.pow(2, counter)) % totalnodes).toInt
+                  println((Integer.parseInt(hashvalue)+scala.math.pow(2, counter)) % totalnodes)
+               //   for( i<- 0 to store_nodenumbers.size){
+                    fingertablemap.addOne(counter,((Integer.parseInt(hashvalue)+scala.math.pow(2, counter)) % totalnodes).toInt)
+              //    val successor_for_key=  (store_nodenumbers.foreach(x => x >= store_temporary_value))
+//             // store_nodenumbers.foreach()
+//              for(e<- store_nodenumbers) {
+//                      if(e>=store_temporary_value){
+//                      val successor_for_key= e
+//                       // println("Server Node is: " + (Integer.parseInt(hashvalue) + "  The key is: " + counter +  " And the calculated Identifier is: "+ store_temporary_value + "Successor for the Node "+successor_for_key) )
+//                        fingertablemap+=(counter->e)
+//                      }
+//                      else{
+//                     //   store_temporary_value = store_temporary_value%2; //hardcoding the total number of nodes in case it goes over our limit
+//                        //(this is the case where we get the identifier which is out of our index. So, 4 is basically our Numoftotalnodes)
+//                        for(e<-store_nodenumbers) {
+//                          if(e>=store_temporary_value){
+//                            val successor_for_key= e
+//                         //   println("2Server Node is: " + (Integer.parseInt(hashvalue) + "  2The key is: " + counter +  " 2 the calculated Identifier is: " + store_temporary_value + "  2Successor for the Node "+successor_for_key) )
+//                            fingertablemap+=(counter->e)
+//                          }
+//                        }
+//                      }
+//                    }
+
+          }
+   //   println(self.path)
+      fingertablemap.foreach( println(_))
+
+
+    }
+    case update_finger_table(nodesnumber) =>{ ///nodesID's (0,1)
+       fingertablemap.foreach{
+        which_key=>
+          val greater_equal = nodesnumber.filter(x=>x>=which_key._2) //._2 represents the identifier
+
+            println("inside update finertable: " + greater_equal + "   " + nodesnumber.size)
+       }
+
     }
     case add_nodering(key,actorPath,ref) =>{
+      store_nodenumbers.addOne(key.toInt)
       actorspath.addOne(actorPath.toString)
       lookup_servernodes.addOne(key->actorPath.toString) //storing server path with key to access (while getting the data)
-      log.info("Node added to the ring")
-      ref!create_fingertable(4)
+     // log.info("Node added to the ring")
+     ref ! create_fingertable(2,key,actorPath) //so we are sending total number of nodes, key and the actorPath
 
+      log.info("Update all the tables")
+      actorspath.foreach{
+        path=>
+          var select_to_update = context.actorSelection(path)
+          select_to_update!update_finger_table(store_nodenumbers)
+      }
     }
+
+
 
     case Insert(key,value) =>{
       map.addOne(key,value)
-      println("Inside the hmap function:  " + self + "    " + map.get(key))
+       println(key + fingertablemap.get(key.toInt))
+    //  println("Inside the hmap function:  " + self + "    " + map.get(key))
     }
     case getValue(key) =>{
       if(map.contains(key)){
-        println("CONTAINS:" + map.get(key))
+      //  println("CONTAINS:" + map.get(key))
       }
       else {
-        println("No key found")
+     //   println("No key found")
       }
     }
+    case get_fingertable()=>{
+    }
+
 
   }
 
   }
-
