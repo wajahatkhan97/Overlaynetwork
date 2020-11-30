f8adde55c708551382ccf1196088155167c6e288
Wajahat Khan <40649143+wajahatkhan97@users.noreply.github.com>
2020-11-29 19:08:24 -0600

    addednewfunctions

--- src/main/scala/lookupdata.scala
+++ src/main/scala/lookupdata.scala
@@ -12,36 +12,36 @@ import scala.math.Ordering.Implicits._
 /*
 //Chord Implementation idea was from "Mr.Abhijeet"  .
 */
-object lookupdata{
-  case class Find_data(key:String , ref:ActorSelection,key_Actor:mutable.HashMap[Int,String],numbernodes:Int) //key value pair
-  case class create_zone_bootstrap(x:Int,y:Int,path:ActorPath)
-  case class create_zone(x:Int,y:Int)
+object lookupdata {
+
+  case class Find_data(key: String, ref: ActorSelection, key_Actor: mutable.HashMap[Int, String], numbernodes: Int) //key value pair
+  case class create_zone_bootstrap(x: Int, y: Int, path: ActorPath)
+
+  case class create_zone(x: Int, y: Int)
 
   var x = 10; //width
   var y = 10; //height
-  var count_nodes_in_bootstrapmap=0
-  var coordinate_zone = new mutable.HashMap[String,ListBuffer[Tuple2[Int,Int]]]()
-  var actual_coordinate_zone = new mutable.HashMap[Tuple2[Int,Int],ListBuffer[Tuple2[Int,Int]]]()
+  var count_nodes_in_bootstrapmap = 0
+  var coordinate_zone = new mutable.HashMap[String, ListBuffer[Tuple2[Int, Int]]]()
+  var actual_coordinate_zone = new mutable.HashMap[String, ListBuffer[Tuple2[Int, Int]]]()
 
-  var idk_coordinate_zone = new mutable.HashMap[Tuple2[Int,Int],ListBuffer[Tuple2[Int,Int]]]()
+  var idk_coordinate_zone = new mutable.HashMap[Tuple2[Int, Int], ListBuffer[Tuple2[Int, Int]]]()
 
-  var actual_coordinate = new mutable.HashMap[String,ListBuffer[Int]]()
+  var actual_coordinate = new mutable.HashMap[String, ListBuffer[Int]]()
 
-  var store_nodes = new mutable.HashMap[Int,String]()
+  var store_nodes = new mutable.HashMap[Int, String]()
 
   //2-D ARRAY
-  val multiarray = Array.ofDim[Int](5,5)
-  for(i<-0 to 4)
-    {
-      for(j<-0 to 4)
-        {
-          multiarray(i)(j)= 0
-        }
+  val multiarray = Array.ofDim[Int](5, 5)
+  for (i <- 0 to 4) {
+    for (j <- 0 to 4) {
+      multiarray(i)(j) = 0
     }
-  def print_Space()= {
-    for(i<-0 to 4)
-    {
-      for(j<-0 to 4) {
+  }
+
+  def print_Space() = {
+    for (i <- 0 to 4) {
+      for (j <- 0 to 4) {
         {
           print(" " + multiarray(i)(j) + " ")
         }
@@ -49,67 +49,31 @@ object lookupdata{
       println()
     }
   }
+
   var list1 = new ListBuffer[Int];
-  var list = new ListBuffer[Tuple2[Int,Int]]; //contains the zones of each node actor including the bootstrap itself
+  var list = new ListBuffer[Tuple2[Int, Int]]; //contains the zones of each node actor including the bootstrap itself
 
-  def find_zone(x:Int,y:Int)={
+  def find_zone(x: Int, y: Int) = {
 
   }
-  def check_zone(list:ListBuffer[Tuple2[Int,Int]],x:Int,y:Int,map:mutable.HashMap[Tuple2[Int,Int],ListBuffer[Tuple2[Int,Int]]]):Int={
-   var count=0;
-    if(map.size ==1){ //meaning we only have coordinates for bootstrap
-      LOGGER.info("Just the bootstrap node simply return from here")
-    }else {
-      for (count <- 0 to 0) {
 
-        map(list(count)).foreach(
-          which_value => {
-            if(which_value._1 < x && which_value._2 > x){
-              LOGGER.info("YES") //so now I know that my new coordinates(point P) resides in which zone
-            }
-          }
-        )
-      }
-    }
-    return count;
-  }
-  def update_zones(list:ListBuffer[Tuple2[Int,Int]], zone:Int,map:mutable.HashMap[Tuple2[Int,Int],ListBuffer[Tuple2[Int,Int]]],x:Int,y:Int ):ListBuffer[Tuple2[Int,Int]]={
-    var new_list = new ListBuffer[Tuple2[Int,Int]]; //contains the zones of each node actor including the bootstrap itself
-    var count=0
-    if(map.size ==1){ //meaning we only have coordinates for bootstrap
-          LOGGER.info("Just the bootstrap node simply return from here")
-            new_list.addOne(0,x)
-          new_list.addOne(0,y)
-          return new_list
-    }
-        else{
-          LOGGER.info("Just the bootstrap node " + map(list(zone)).head)
+  def check_zone(x:Int,y:Int,store_nodes:mutable.HashMap[Int, String],actual_Cordinates:mutable.HashMap[String, ListBuffer[Tuple2[Int, Int]]]) = {
+    /*
+    So x represents the new nodes X point and Y represents the Y point
+    store_nodes have the path of each node via which
+    actual coordinates have the actual zones where node path is the key
+     -----implementation-------
+        check which path contains the zone return the path and in update zone update the map using the
+     */
 
-      map(list(zone)).foreach(
-        which_value => {
-          if(which_value._1 < x && which_value._2 > x && count ==0){
-            LOGGER.info("YES") //so now I know that my new coordinates(point P) resides in which zone
-            count+1;
-            new_list.addOne(which_value._1 ,x)
-          }
-          else{
-            if(which_value._1 < y && which_value._2 > y && count ==1){
-              LOGGER.info("YES") //so now I know that my new coordinates(point P) resides in which zone
-              new_list.addOne(which_value._1 ,y)
-              count+1;
-            }
-          }
 
-        }
-      )
-      map.put(list(zone),new_list)
-    //issue I'm having with this is that I'm unable to update my new node zones but update the zone of previous node
-    }
-    return new_list
+
   }
 
+  def update_zones() = {
+
+  }
 }
-
 /*
 CAN Implemetation approach
 */
@@ -142,21 +106,28 @@ class lookupdata extends Actor with ActorLogging {
         Also, make sure to check we don't cross other nodes zone  (check their zones and compare with yours using the map that stores all the info)
        */
       LOGGER.info(Integer.toString(multiarray(x)(y)))
-      list.addOne(x,y)
+      if(count_nodes_in_bootstrapmap==0) { //just only once
+        list.addOne(0, 4) //4x4 x
+        list.addOne(0, 4)//y
 
-      coordinate_zone.put(path.toString,list)
-      list1.addOne((0,x))
-      list1.addOne((0,y)) //this list have the bootstrap node zone
-      actual_coordinate_zone.put(list(count_nodes_in_bootstrapmap),list1) //this map will maintain the zones
+        actual_coordinate_zone.put(path.toString, list) //this list will be the one to get updated
+        store_nodes.put(count_nodes_in_bootstrapmap, path.toString()) //store the nodes
 
-      var zone=  check_zone(list,x,y,actual_coordinate_zone) //returns the zone
+      }
+      if(count_nodes_in_bootstrapmap>0) {
+        store_nodes.put(count_nodes_in_bootstrapmap, path.toString()) //store the nodes
+        list1.addOne((0, x))
+        list1.addOne((0, y)) //this list have the bootstrap node zone
+        //In check zone where this current coordinates lands
+        check_zone(x,y,store_nodes,actual_coordinate_zone) //returns the zone
 
-      //okay so we will have one more map whose key will be the coordinates and it contains tuples of zone limits
-     var newlist= update_zones(list,zone,actual_coordinate_zone,x,y) //update the zone
 
-      LOGGER.info("see" + actual_coordinate_zone)
+        update_zones() //update the zone
+        actual_coordinate_zone.put(path.toString, list1) //this list will be the one to get updated
 
-      store_nodes.put(count_nodes_in_bootstrapmap,path.toString()) //store the nodes
+        LOGGER.info("Bootstrap" + coordinate_zone)
+        LOGGER.info("Nodes" + actual_coordinate_zone)
+      }
       //LOGGER.info("So How many nodes we have now in the list returned by bootstrap: " + list )
       count_nodes_in_bootstrapmap+=1;
 
